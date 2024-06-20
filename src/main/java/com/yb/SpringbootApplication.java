df1189d7bfea91a913c97915481d288062bf5ae1
liming <522724933@qq.com>
2024-06-20 18:03:12 +0800

    #日志添加启动成功

--- src/main/java/com/yb/SpringbootApplication.java
+++ src/main/java/com/yb/SpringbootApplication.java
@@ -1,17 +1,19 @@
 package com.yb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
+@Slf4j
 @EnableSwagger2WebMvc
 @SpringBootApplication
 @EnableScheduling
-public class MySpringbootApplication {
+public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySpringbootApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
+        log.info(">>>>>>>>>>>>>>>>>> {}", SpringbootApplication.class.getSimpleName().toUpperCase() + " STARTING SUCCESS");
     }
 
 }
