610a974bf80bb7140d350c0e90119b75e17c9128
liming <522724933@qq.com>
2024-06-25 10:25:41 +0800

    #列表

--- src/main/java/com/yb/controller/HelloController.java
+++ src/main/java/com/yb/controller/HelloController.java
@@ -1,22 +1,17 @@
 package com.yb.controller;
 
 
-import com.yb.entity.Test;
-import com.yb.service.ITestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HelloController {
 
-    private final ITestService iTestService;
 
     @ResponseBody
     @RequestMapping("/hello")
@@ -25,10 +20,6 @@ public class HelloController {
         return "hello word!";
     }
 
-    @ResponseBody
-    @RequestMapping("/test")
-    public List<Test> test() {
-        return iTestService.selectAll();
-    }
+
 
 }
